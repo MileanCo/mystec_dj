bc215668b3f0f12bae9163fe98284827b7c1ce26
mystec <mystecstudios@gmail.com>
2016-06-05 17:08:08 -0400

    Added sendmail capability from UI using Parse backend

--- js/sendMail.js
+++ js/sendMail.js
@@ -2,8 +2,38 @@
 function sendMail()
 {
     var yourMessage = document.getElementById("message").value;
-    var subject = document.getElementById("firstname").value;
+    var yourEmail = document.getElementById("email").value || "";
+    var subject = document.getElementById("firstname").value || "";
+    if (!yourMessage) {
+      alert("No message provided for email");
+      return "";
+    }
+
+    $.ajax({
+      type: "POST",
+      //url: "http://golocoapp.com:1337/parse/functions/get_events",
+      url: "http://localhost:1337/parse/functions/send_mail",
+      headers: {
+        "X-Parse-Application-Id": "g0l0c0",
+      },
+      data: {
+        to: "mystecstudios@gmail.com",
+        from: yourEmail,
+        subject: "DJ Mystec - Site Lead from " + subject,
+        text: yourMessage,
+        html: yourMessage,
+      },
+      success: function(data, textStatus, jqXHR) {
+        console.log(data);
+      },
+      dataType: "json",
+    });
+
+    console.log("Sending mail to " + subject);
+
+    /**
     document.location.href = "mailto:joelm.sten@gmail.com?subject="
         + encodeURIComponent(subject)
         + "&body=" + encodeURIComponent(yourMessage);
+    */
 }
